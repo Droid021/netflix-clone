ea810a2aa34d051c4f23120d45fa310ada01a96c
Droid021 <vincivenv@gmail.com>
2020-09-25 10:55:57 +0300

    accordion toggle state,create and use context

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
+// jumbotron component expose
 export { default as Jumbotron } from './jumbotron'
 
+// expose footer component
 export { default as Footer } from './footer';
+
+// expose accordion component
+export { default as Accordion } from './accordion';
