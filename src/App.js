7b59ea3e5451eae41a8fced4d77205370548408f
Droid021 <vincivenv@gmail.com>
2020-10-06 18:36:20 +0300

    feat: Firebase auth listener

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { Home, Browse, Signin, Signup } from './pages';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 
 export default function App() {
-  const user = null;
+  const { user } = useAuthListener();
+
+  console.log(user);
 
   return (
     <Router>
