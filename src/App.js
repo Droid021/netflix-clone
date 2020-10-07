dfda7e18ca62c2d16d519ba7a7c2e54465451d1b
Droid021 <vincivenv@gmail.com>
2020-10-07 12:02:44 +0300

    Fetch series and films, custom hook

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Home, Browse, Signin, Signup } from './pages';
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 import { useAuthListener } from './hooks';
@@ -10,25 +10,25 @@ import { useAuthListener } from './hooks';
 export default function App() {
   const { user } = useAuthListener();
 
-  console.log(user);
-
   return (
     <Router>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
-        <Signin />
-      </IsUserRedirect>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
+          <Signin />
+        </IsUserRedirect>
 
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
-        <Signup />
-      </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
+          <Signup />
+        </IsUserRedirect>
 
-      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
-        <Browse />
-      </ProtectedRoute>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+          <Browse />
+        </ProtectedRoute>
 
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
-        <Home />
-      </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { useContent } from '../hooks';
 
 export default function Browse() {
+  // slides
+  // films, series
+  // pass to browse container
+  const { series } = useContent('series');
+  console.table(series);
+
   return <h1>Browse</h1>;
 }
